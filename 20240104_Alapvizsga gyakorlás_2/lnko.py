bcec9d75e6a6b2c2cc86d11e68adbf69ccc4f251
M4rkjk <144492665+M4rkjk@users.noreply.github.com>
2024-01-15 17:37:24 +0100

    Add files via upload

---
+++ 20240104_Alapvizsga gyakorlás_2/lnko.py
@@ -0,0 +1,19 @@
+print('LNKO kivonásos algoritmussal')
+a = int(input('a = '))
+b = int(input('b = '))
+
+
+if a > b:
+    nagyobb = a
+    kisebb = b
+else:
+    nagyobb = b
+    kisebb = a
+
+
+while nagyobb != kisebb:
+    nagyobb -= kisebb
+    if nagyobb < kisebb:
+        nagyobb, kisebb = kisebb, nagyobb
+
+print(f'LNKO({a}, {b}) = {nagyobb}')
