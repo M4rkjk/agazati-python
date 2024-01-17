51916f476986a7fda8f7abf5909774c9a915618d
Mark <144492665+M4rkjk@users.noreply.github.com>
2024-01-17 15:29:12 +0100

    Delete 20240116_Gyakorlás directory

--- 20240116_Gyakorlás/jelszo.py
+++
@@ -1,9 +0,0 @@
-user = ''
-pw = ''
-while user != 'bori99' and pw != 'Szivecske<3':
-    user = input('Adja meg a felhasználónevét: ')
-    pw = input('Adja meg a jelszavát: ')
-    if user != 'bori99' and pw != 'Szivecske<3':
-        print('Belépés megtagadva.')
-print('Belépés engedélyezve!')
-    
--- 20240116_Gyakorlás/nagyobb.py
+++
@@ -1,8 +0,0 @@
-elso_szam = int(input('Adja meg az első számot: '))
-masodik_szam = int(input('Adja meg a második számot: '))
-if elso_szam > masodik_szam:
-    print(f'{elso_szam} nagyobb, mint a {masodik_szam}')
-elif elso_szam < masodik_szam:
-    print(f'{elso_szam} kisebb, mint a {masodik_szam}')
-else:
-    print(f'{elso_szam} és {masodik_szam} egyenlő')
